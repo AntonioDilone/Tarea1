8d2861ca847bf8e3b15ed652a5ab45ce2d033381
Antonio Dilone <antoniodilone26@gmail.com>
2023-02-05 15:33:59 -0400

    Calculadora de areas

---
+++ Calculadora de areas/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_areas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula areas en cm^2");
+            Console.WriteLine("0- Cuadrado  1- Circulo  2- Triangulo  3- Pentagono  4- Hexagono");
+            int opcion = int.Parse(Console.ReadLine());
+            double area;
+
+            if (opcion == 0) {
+                Console.WriteLine("Ingrese la medida de un lado");
+                int lado = int.Parse(Console.ReadLine());
+                area = lado * lado;
+                Console.WriteLine("El area es de " + area + "cm^2");
+            } 
+            
+            else if (opcion == 1)
+            {
+                Console.WriteLine("Ingrese el radio de su circulo");
+                int radio = int.Parse(Console.ReadLine());
+                area = 3.14 * (radio * radio);
+                Console.WriteLine("El area es de " + area + "cm^2");
+            } 
+            
+            else if (opcion == 2)
+            {
+                Console.WriteLine("Ingrese la base");
+                int bas = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la altura");
+                int altura = int.Parse(Console.ReadLine());
+                area = (bas * altura) / 2;
+                Console.WriteLine("El area es de " + area + "cm^2");
+            } 
+            
+            else if (opcion == 3)
+            {
+                Console.WriteLine("Ingrese la medida de un lado");
+                int lado = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la medida del apotema");
+                int apotema = int.Parse(Console.ReadLine());
+                double per = lado * 5;
+                area = (per * apotema) /2;
+
+                Console.WriteLine("El area es de " + area + "cm^2");
+            } 
+            
+            else if (opcion == 4)
+            {
+                Console.WriteLine("Ingrese la medida de un lado");
+                int lado = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la medida del apotema");
+                int apotema = int.Parse(Console.ReadLine());
+                double per = lado * 5;
+                area = (per * apotema) / 2;
+
+                Console.WriteLine("El area es de " + area + "cm^2");
+            } 
+            
+            else {
+                Console.WriteLine("Ingrese una opcion valida");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Calculadora de areas/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Calculadora de areas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculadora de areas")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("15db7f6d-8d7c-4166-ae86-46ba9feab973")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
