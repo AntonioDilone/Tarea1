d9dedb4c24655516ad445116611195560882d7fe
anton <anton@LAPTOP-QM5FFGMO>
2023-02-05 10:00:30 -0400

    Agregar archivos de proyecto.

---
+++ Tarea1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula el  factorial de un numero");
+            Console.WriteLine("Coloque un numero");
+            int numero = int.Parse(Console.ReadLine());
+            for (i =  0, ) { 
+            }
+
+
+        }
+    }
+}
