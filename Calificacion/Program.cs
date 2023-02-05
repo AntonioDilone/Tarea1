e59018132000365c2649451ed53210fd2c3f655c
Antonio Dilone <antoniodilone26@gmail.com>
2023-02-05 19:51:05 -0400

    Calificaciones

---
+++ Calificacion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular calificacion promedio");
+            Console.WriteLine("Cuantas calificaciones quiere añadir?");
+            int cant = int.Parse(Console.ReadLine());
+            int promedio = 0;
+
+            List<int> calificaciones = new List<int>();
+
+            for (int i = 0; i < cant; i++)
+            {
+                Console.WriteLine("Añada una nota");
+                calificaciones.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Nota añadida con exito");
+            }
+
+            foreach (var item in calificaciones)
+            {
+                promedio += item;
+            }
+            promedio = promedio / calificaciones.Count;
+            Console.WriteLine("El promedio es de " + promedio);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Temperaturas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperaturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convertir temperaturas");
+            Console.WriteLine("1-Celcius 2-Kelvin 3-Fahrenheit");
+            int opcion 
+        }
+    }
+}
